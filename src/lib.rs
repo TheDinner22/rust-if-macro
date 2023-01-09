90239cf72b752678395305bb24fe6a4ecf47b545
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-01-08 19:07:38 -0500

    updated syntax+better main example code

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,4 @@
 #[macro_export]
 macro_rules! my_if {
-    ($bool:expr => $code:block) => {
-        match $bool {
-            true => {$code},
-            false => {/* cond was false, ignoring input */},
-            _ => panic!("expected bool, got {:#?}", stringify!($bool))
-        }
-    };
-    
-    ($bool:expr => $if_code:block otherwise: $else_code:block) => {
-        match $bool {
-            true => {$if_code},
-            false => {$else_code},
-            _ => panic!("expected bool, got {:#?}", stringify!($bool))
-        }
-    };
+    ($if_bool:expr => $if_code:block $(elif $elif_bool:expr => $elif_code:block)* $(otherwise => $else_code:block)?) => {};
 }
-
