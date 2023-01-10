50edde803ab274048cfb256782d12a504cf6e785
joe goodman <jgood1133@gmail.com>
2023-01-10 15:35:28 -0500

    working with compromised syntax, next need to add else feature

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,7 +5,9 @@
 use cond_macros::*;
 
 fn main() {
-    my_if!(a==1 => {
+    let mut a = 4;
+
+    my_if!(elif a==1 => {
       a = 111;
     }
     elif a==2 => {
@@ -17,27 +19,6 @@ fn main() {
     otherwise => {
         a = 123;
     });
-
-    // i want this expansion to look like
-    let mut a = 1;
-    {
-        match a == 1 {
-            true => {
-                a = 111;
-            }
-            false => match a == 2 {
-                true => {
-                    a = 222;
-                }
-                false => match a == 3 {
-                    true => {
-                        a = 333;
-                    }
-                    false => a = 123,
-                },
-            },
-        }
-    }
 
     println!("{a}");
 }
