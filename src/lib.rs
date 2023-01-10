9d14f2ab73dc97b5bb8353e00649aee6938297c2
joe goodman <jgood1133@gmail.com>
2023-01-10 14:58:40 -0500

    work on recursive macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,21 @@
 #[macro_export]
 macro_rules! my_if {
-    ($if_bool:expr => $if_code:block $(elif $elif_bool:expr => $elif_code:block)* $(otherwise => $else_code:block)?) => {};
+    ($if_bool:expr => $if_code:block $(elif $elif_bool:expr => $elif_code:block)* $(otherwise => $else_code:block)?) => {
+        match $if_bool {
+            true => $if_code,
+            false => {my_if!(
+                elif_chain_to_if_elif_chain!(
+                    $(elif $elif_bool => $elif_code)*
+                )
+            );}
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! elif_chain_to_if_elif_chain {
+    () => {};
+    (elif $first_elif_bool:expr => $first_elif_code:block $(elif $elif_bool:expr => $elif_code:block)*) => {
+        $first_elif_bool => $first_elif_code $(elif $elif_bool:expr => $elif_code:block)*
+    };
 }
